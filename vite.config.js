8f11beb5d706618ca19b85bcb07592cc3d1cfaf5
Leonardo <leonardosm@protonmail.com>
2026-06-02 09:51:26 -0300

    Fix whitespace in API proxy target URL

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: ' https://api-powergym.onrender.com/',
+        target: 'https://api-powergym.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
